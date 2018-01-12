c95c50c6316ff22ae874f344f381b03750d5a2b7
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-01-12 12:38:23 +0100

    test_flag_d add some tests

--- src/tests/test_flag_d.c
+++ src/tests/test_flag_d.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 11:50:42 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/01/12 12:32:27 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/01/12 12:36:52 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,16 @@ int		ft_test_flag_d(void)
 	STOPIF0(ft_call_both("Now number is %3d", 123456789));
 	STOPIF0(ft_call_both("I changed the number : %3d", -10));
 	STOPIF0(ft_call_both("Again : %3d...", -123456789));
+
+	STOPIF0(ft_call_both("Le nombre est %#+-0 123.4d !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *d !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4d !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4hd !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4hhD !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4lu !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4llU !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4jC !\n"));
+	STOPIF0(ft_call_both("Le nombre est %#+-0 *.4zp !\n"));
 
 	printf("*** Flag d : all tests passed ***\n");
 	return (1);
