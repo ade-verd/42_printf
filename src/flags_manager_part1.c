c07d07c4a0bffe9f658c4bacf394d68313cc24c4
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-01-16 18:04:26 +0100

    cut flags_managers in two parts

--- src/flags_manager_part1.c
+++ src/flags_manager_part1.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   indicators_manager.c                               :+:      :+:    :+:   */
+/*   flags_manager_part1.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/11 12:58:47 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/01/16 17:55:07 by ade-verd         ###   ########.fr       */
+/*   Created: 2018/01/16 17:58:47 by ade-verd          #+#    #+#             */
+/*   Updated: 2018/01/16 18:02:45 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_flags_manager(t_indic **ind, t_buff **buff, char **str)
-{
-	ft_print_sign_before(ind, buff, str);
-	ft_manage_plus(ind, buff, str);
-	ft_manage_minus(ind, buff, str);
-	ft_manage_zero(ind, buff, str);
-}
+static const t_hash	g_ft[] = {
+	{"oxX", ft_int_type_},
+	{"eEfFaA", ft_int_type_},
+	{"gG", ft_int_type_},
+	{ 0, 0}
+};
 
-void	ft_indicators_manager(t_indic **ind, t_buff **buff, char **str)
+/*
+** ft_manage_hashtag
+*/
+
+void	ft_manage_hashtag(t_indic **ind, t_buff **buff, char **str)
 {
-	ft_flags_manager(ind, buff, str);
 }
