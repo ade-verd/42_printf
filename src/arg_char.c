0a89e205a4ab5b94544222e2d0bbf09cc80c0331
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-02-13 18:37:51 +0100

    %c changed to fit with %s

--- src/arg_char.c
+++ src/arg_char.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/08 17:52:58 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/02/13 16:11:13 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/02/13 18:29:23 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,34 +30,65 @@ void    ft_iscapital_c(t_indic **ind)
     }
 }
 
-void 	ft_convert_unicode(t_indic **ind, char **byte)
+void 	ft_convert_unicode(t_indic **ind, char **byte, int *i)
 {
 	int 	nb_bits;
 
 	nb_bits = ft_bitlen((*ind)->c);
 	if (nb_bits <= 7)
-		(*byte)[0] = (*ind)->c;
+		(*byte)[(*i)++] = (*ind)->c;
 	else if (nb_bits <= 11)
 	{
-		(*byte)[0] = MASK_BYTE01 | ((*ind)->c >> 6 & RMASK_BYTE01);
-		(*byte)[1] = MASK_BYTE10 | ((*ind)->c & RMASK_BYTE10);
+		(*byte)[(*i)++] = MASK_BYTE01 | ((*ind)->c >> 6 & RMASK_BYTE01);
+		(*byte)[(*i)++] = MASK_BYTE10 | ((*ind)->c & RMASK_BYTE10);
 	}
 	else if (nb_bits <= 16)
 	{
-		(*byte)[0] = MASK_BYTE02 | ((*ind)->c >> 12 & RMASK_BYTE02);
-		(*byte)[1] = MASK_BYTE10 | ((*ind)->c >> 6 & RMASK_BYTE10);
-		(*byte)[2] = MASK_BYTE10 | ((*ind)->c & RMASK_BYTE10);
+		(*byte)[(*i)++] = MASK_BYTE02 | ((*ind)->c >> 12 & RMASK_BYTE02);
+		(*byte)[(*i)++] = MASK_BYTE10 | ((*ind)->c >> 6 & RMASK_BYTE10);
+		(*byte)[(*i)++] = MASK_BYTE10 | ((*ind)->c & RMASK_BYTE10);
 	}
 	else
 	{
-		(*byte)[0] = MASK_BYTE03 | ((*ind)->c >> 18 & RMASK_BYTE03);
-		(*byte)[1] = MASK_BYTE10 | ((*ind)->c >> 12 & RMASK_BYTE10);
-		(*byte)[2] = MASK_BYTE10 | ((*ind)->c >> 6 & RMASK_BYTE10);
-		(*byte)[3] = MASK_BYTE10 | ((*ind)->c & RMASK_BYTE10);
+		(*byte)[(*i)++] = MASK_BYTE03 | ((*ind)->c >> 18 & RMASK_BYTE03);
+		(*byte)[(*i)++] = MASK_BYTE10 | ((*ind)->c >> 12 & RMASK_BYTE10);
+		(*byte)[(*i)++] = MASK_BYTE10 | ((*ind)->c >> 6 & RMASK_BYTE10);
+		(*byte)[(*i)++] = MASK_BYTE10 | ((*ind)->c & RMASK_BYTE10);
 	}
 }
 
+void 	ft_unicode_to_str(t_indic **ind, char **str, wchar_t c, int *i)
+{
+	(*ind)->c = c;
+	if ((*ind)->size && ft_strcmp((*ind)->size, "l") == 0)
+		ft_convert_unicode(ind, str, i);
+	else
+		(*str)[(*i)++] = (*ind)->c;
+}
+
 void	ft_arg_char(va_list ap, t_indic **ind, t_buff **buff)
+{
+	char 	*str;
+	int		iszero;
+	int 	i;
+
+	i = 0;
+	str = ft_strnew(4);
+	ft_bzero(str, 4);
+    ft_iscapital_c(ind);
+	ft_get_string(ind, ap, &str);
+	ft_unicode_to_str(ind, &str, (*ind)->c, &i);
+	iszero = (*ind)->c == 0 ? 1 : 0;
+	if (iszero == 1)
+		str[0] = '.';
+	ft_indicators_manager(ind, buff, &str);
+	iszero == 1 ? ft_putcbuffer(buff, 0) : ft_putsbuffer(buff, str);
+	if ((*buff)->suffix)
+		ft_putsbuffer(buff, (*buff)->suffix);
+	ft_strdel(&str);
+}
+
+/*void	ft_arg_char(va_list ap, t_indic **ind, t_buff **buff)
 {
 	char 	*str;
 	int		iszero;
@@ -79,4 +110,4 @@ void	ft_arg_char(va_list ap, t_indic **ind, t_buff **buff)
 	if ((*buff)->suffix)
 		ft_putsbuffer(buff, (*buff)->suffix);
 	ft_strdel(&str);
-}
+}*/
