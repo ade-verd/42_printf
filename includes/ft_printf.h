e1c8d0d702f50cc2567164fce4dcbfbb0db7919b
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-02-21 13:23:26 +0100

    norminette ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 17:22:06 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/02/20 20:23:07 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/02/21 13:16:18 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,35 @@
 # include <stdlib.h>
 # include <limits.h>
 # include <stdint.h>
-# include <stddef.h> /*ptrdiff_t*/
+# include <stddef.h>
 # include "libft.h"
-# include <stdio.h> /* /!\ /!\ A SUPPRIMER /!\ /!\ */
+
+# include <stdio.h> /* A SUPPRIMER */
 
 # define BUFF_MAX_SIZE	4096
 
-/*Unicode masks*/
-# define MASK_BYTE00	0x0		/*0xxx xxxx*/
-# define RMASK_BYTE00	0x7F	/*0111 1111*/
+/*
+** Unicode masks
+**	MASK_BYTE00		0x0		0xxx xxxx	||	RMASK_BYTE00	0x7F	0111 1111
+**	MASK_BYTE01		0xC0	110x xxxx	||	RMASK_BYTE01	0x1F	0001 1111
+**	MASK_BYTE02		0xE0	1110 xxxx	||	RMASK_BYTE02	0x0F	0000 1111
+**	MASK_BYTE03		0xF0	1111 0xxx	||	RMASK_BYTE03	0x07	0000 0111
+**	MASK_BYTE10		0x80	10xx xxxx	||	RMASK_BYTE10	0x3F	0011 1111
+*/
+# define MASK_BYTE00	0x0
+# define RMASK_BYTE00	0x7F
+# define MASK_BYTE01	0xC0
+# define RMASK_BYTE01	0x1F
+# define MASK_BYTE02	0xE0
+# define RMASK_BYTE02	0x0F
+# define MASK_BYTE03	0xF0
+# define RMASK_BYTE03	0x07
+# define MASK_BYTE10	0x80
+# define RMASK_BYTE10	0x3F
 
-# define MASK_BYTE01	0xC0	/*110x xxxx*/ 
-# define RMASK_BYTE01	0x1F	/*0001 1111*/ 
-
-# define MASK_BYTE02	0xE0	/*1110 xxxx*/
-# define RMASK_BYTE02	0x0F	/*0000 1111*/
-
-# define MASK_BYTE03	0xF0	/*1111 0xxx*/
-# define RMASK_BYTE03	0x07	/*0000 0111*/
-
-# define MASK_BYTE10	0x80	/*10xx xxxx*/
-# define RMASK_BYTE10	0x3F	/*0011 1111*/
-
-/*Buffer*/
+/*
+** Buffer
+*/
 typedef struct		s_buff
 {
 	char			*str;
@@ -52,10 +58,12 @@ typedef struct		s_buff
 	int				index;
 	int				total;
 	int				fd;
-	int 			err;
+	int				err;
 }					t_buff;
 
-/*Indicators*/
+/*
+** Indicators
+*/
 typedef struct		s_indic
 {
 	char			*flags;
@@ -69,7 +77,92 @@ typedef struct		s_indic
 	wchar_t			*ws;
 }					t_indic;
 
-/*Associated function types and pointers*/
+/*
+** ft_printf and cie
+*/
+int					ft_printf(const char *str, ...);
+int					ft_dprintf(int fd, const char *str, ...);
+int					ft_vprintf(const char *str, va_list ap);
+int					ft_vdprintf(int fd, const char *str, va_list ap);
+
+/*
+** Struct init and free
+*/
+void				ft_init_indic(t_indic **ind);
+void				ft_free_indic(t_indic **ind);
+void				ft_init_buffer(t_buff **buff, int fd);
+void				ft_free_buff(t_buff **buff);
+void				ft_reset_struct(t_indic **ind, t_buff **buff);
+
+/*
+** Printer
+*/
+void				ft_putcbuffer(t_buff **buff, char c);
+void				ft_putsbuffer(t_buff **buff, char *str);
+void				ft_print_buffer(t_buff **buff);
+
+/*
+** Get chars to convert
+*/
+void				ft_get_all_indics(t_indic **in, va_list a, char *s, int *i);
+void				ft_get_flags(t_indic **ind, char *str, int *i);
+void				ft_get_width(t_indic **ind, va_list ap, char *str, int *i);
+void				ft_get_precision(t_indic **in, va_list ap, char *s, int *i);
+void				ft_get_size(t_indic **ind, char *str, int *i);
+void				ft_get_type(t_indic **ind, char *str, int *i);
+
+/*
+** Parse
+*/
+void				ft_parse_str(va_list ap, char *str, int *ret, int fd);
+
+/*
+** Convert
+*/
+void				ft_indicators_manager(t_indic **i, t_buff **buff, char **s);
+void				ft_arg_int(va_list ap, t_indic **ind, t_buff **buff);
+void				ft_arg_char(va_list ap, t_indic **ind, t_buff **buff);
+void				ft_arg_string(va_list ap, t_indic **ind, t_buff **buff);
+void				ft_convert_unicode(t_indic **ind, char **byte, int *i);
+
+/*
+** Flags
+*/
+void				ft_print_sign_before(t_indic **i, t_buff **buff, char **s);
+void				ft_manage_hashtag(t_indic **ind, t_buff **buff, char **str);
+void				ft_manage_zero(t_indic **ind, t_buff **buff, char **str);
+void				ft_manage_plus_signed(t_indic **i, t_buff **b, char **s);
+void				ft_manage_plus_unsigned(t_indic **i, t_buff **b, char **s);
+void				ft_manage_plus_string(t_indic **ind, t_buff **b, char **s);
+void				ft_manage_minus(t_indic **ind, t_buff **buff, char **str);
+
+/*
+** Precision
+*/
+void				ft_precision_int(t_indic **ind, t_buff **buff, char **str);
+void				ft_precision_string(t_indic **i, t_buff **buff, char **str);
+
+/*
+** Size
+*/
+void				ft_get_string(t_indic **ind, va_list ap, char **str);
+void				ft_get_chars_csp(t_indic **ind, va_list ap);
+
+/*
+** Unicode
+*/
+void				ft_unicode_to_str(t_indic **ind,
+									t_buff **b, char **s, int *i);
+
+/*
+** Errors
+*/
+void				ft_error_manager(t_buff **buff);
+void				ft_arg_novalid_conv(t_indic **i, t_buff **b);
+
+/*
+** Associated function types and pointers
+*/
 typedef struct		s_conv
 {
 	char			*letters;
@@ -84,65 +177,5 @@ typedef struct		s_ft
 	void			(*f3)(t_indic**, va_list, char **str, int base);
 	void			(*f4)(t_indic**, va_list, char **str);
 }					t_ft;
-
-/*ft_printf and cie*/
-int					ft_printf(const char *str, ...);
-int					ft_dprintf(int fd, const char *str, ...);
-int					ft_vprintf(const char *str, va_list ap);
-int					ft_vdprintf(int fd, const char *str, va_list ap);
-
-/*Struct init and free*/
-void				ft_init_indic(t_indic **ind);
-void				ft_free_indic(t_indic **ind);
-void				ft_init_buffer(t_buff **buff, int fd);
-void				ft_free_buff(t_buff **buff);
-void				ft_reset_struct(t_indic **ind, t_buff **buff);
-
-/*Printer*/
-void				ft_putcbuffer(t_buff **buff, char c);
-void				ft_putsbuffer(t_buff **buff, char *str);
-void				ft_print_buffer(t_buff **buff);
-
-/*Get chars to convert*/
-void				ft_get_all_indics(t_indic **in, va_list a, char *s, int *i);
-void				ft_get_flags(t_indic **ind, char *str, int *i);
-void				ft_get_width(t_indic **ind, va_list ap, char *str, int *i);
-void				ft_get_precision(t_indic **in, va_list ap, char *s, int *i);
-void				ft_get_size(t_indic **ind, char *str, int *i);
-void				ft_get_type(t_indic **ind, char *str, int *i);
-
-/*Parse*/
-void				ft_parse_str(va_list ap, char *str, int *ret, int fd);
-
-/*Convert*/
-void				ft_indicators_manager(t_indic **i, t_buff **buff, char **s);
-void				ft_arg_int(va_list ap, t_indic **ind, t_buff **buff);
-void				ft_arg_char(va_list ap, t_indic **ind, t_buff **buff);
-void				ft_arg_string(va_list ap, t_indic **ind, t_buff **buff);
-void 				ft_convert_unicode(t_indic **ind, char **byte, int *i);
-
-/*Flags*/
-void				ft_print_sign_before(t_indic **i, t_buff **buff, char **s);
-void				ft_manage_hashtag(t_indic **ind, t_buff **buff, char **str);
-void				ft_manage_zero(t_indic **ind, t_buff **buff, char **str);
-void				ft_manage_plus_signed(t_indic **i, t_buff **b, char **s);
-void				ft_manage_plus_unsigned(t_indic **i, t_buff **b, char **s);
-void				ft_manage_plus_string(t_indic **ind, t_buff **buff, char **str);
-void				ft_manage_minus(t_indic **ind, t_buff **buff, char **str);
-
-/*Precision*/
-void				ft_precision_int(t_indic **ind, t_buff **buff, char **str);
-void				ft_precision_string(t_indic **i, t_buff **buff, char **str);
-
-/*Size*/
-void				ft_get_string(t_indic **ind, va_list ap, char **str);
-void				ft_get_chars_csp(t_indic **ind, va_list ap);
-
-/*Unicode*/
-void 				ft_unicode_to_str(t_indic**, t_buff **b, char **s, int *i);
-
-/*Errors*/
-void				ft_error_manager(t_buff **buff);
-void				ft_arg_novalid_conv(t_indic **i, t_buff **b);
 
 #endif
